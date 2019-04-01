8f5d18b6d837d239d417337ea227655ff8f99280
Kristian <krdo@eaaa.dk>
2019-04-01 15:23:38 +0200

    Commiting MERN template for Heroku deployment

---
+++ express/app.js
@@ -0,0 +1,48 @@
+/**** External libraries ****/
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+/**** Configuration ****/
+const appName = "Foobar";
+const port = (process.env.PORT || 8080);
+const app = express();
+app.use(bodyParser.json()); // Parse JSON from the request body
+app.use(morgan('combined')); // Log all requests to the console
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Additional headers for the response to avoid trigger CORS security
+// errors in the browser
+// Read more here: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+    // intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      // respond with 200
+      console.log("Allowing OPTIONS");
+      res.send(200);
+    }
+    else {
+      // move on
+      next();
+    }
+});
+
+/**** Routes ****/
+app.get('/api/hello', (req, res) => res.json({msg: "Hello from the API"}));
+
+/**** Reroute all unknown requests to react index.html ****/
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+/**** Start! ****/
+app.listen(port, () => console.log(`${appName} API running on port ${port}!`));
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    API_URL = process.env.REACT_APP_API_URL;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            timer: 5,
+            data: "not loaded"
+        };
+
+        this.handleCountdown = this.handleCountdown.bind(this);
+        this.getData = this.getData.bind(this);
+    }
+
+    componentDidMount() {
+        setTimeout(this.handleCountdown, 1000);
+    }
+
+    handleCountdown() {
+        if (this.state.timer > 0)
+        {
+            this.setState({
+                timer: this.state.timer - 1
+            });
+            setTimeout(this.handleCountdown, 1000);
+        } else {
+            this.getData();
+        }
+    }
+
+    getData() {
+        fetch(`${this.API_URL}/hello`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    data: data.msg
+                });
+            })
+            .catch(error => {
+                console.error("Error when fetching: ", error);
+            })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>MERN Deployment Example</h1>
+
+                <p>Countdown to API call: {this.state.timer}</p>
+
+                <p>Data: {this.state.data}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
